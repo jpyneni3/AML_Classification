f89c900909a707ee8a78c738080575f53b7fa207
GulayBenguUlukaya <61863714+GulayBenguUlukaya@users.noreply.github.com>
2021-02-21 22:27:36 -0500

    Add files via upload

---
+++ ML_RandomForest.py
@@ -0,0 +1,63 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.ensemble
+import sklearn.preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+import seaborn as sns
+
+X_train = pd.read_csv('x_train.csv',header=0, index_col=0)
+print(X_train.head())
+
+
+y_train = pd.read_csv('y_train.csv',header=0, index_col=0)
+print(y_train.head())
+
+X_test = pd.read_csv('x_val.csv',header=0, index_col=0)
+print(X_test.head())
+
+y_test = pd.read_csv('y_val.csv',header=0, index_col=0)
+print(y_test.head())
+
+#Import Random Forest Model
+from sklearn.ensemble import RandomForestClassifier
+
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+print(clf.predict_proba(X_test))
+
+train_data = pd.read_csv('train_mean_features_labelled.csv',header=0, index_col=0)
+
+X_train = train_data.loc[:, train_data.columns != 'label']
+y_train = train_data.loc[:, train_data.columns == 'label']
+
+X_test = pd.read_csv('test_mean_features.csv',header=0, index_col=0)
+
+print(X_train.head())
+print(y_train.head())
+print(X_test.head())
+
+#Import Random Forest Model
+from sklearn.ensemble import RandomForestClassifier
+
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+
+print(sum(clf.predict(X_test)))
+print(len(clf.predict(X_test)))
+print(clf.predict_proba(X_test))
